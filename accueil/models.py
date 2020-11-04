56a8f724a97015f265871368a02455bcd2af156a
Sylvain <boussignac.s@gmail.com>
2020-11-04 20:39:39 +0100

    strucure bdd

--- accueil/models.py
+++ accueil/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 
+
+class Regions(models.Model):
+    numreg = models.IntegerField(primary_key=True)
+    libreg = models.CharField(max_length=50, null=False)
+
+
+class Departements(models.Model):
+    numdep = models.IntegerField(primary_key=True)
+    libdep = models.CharField(max_length=50, null=False)
+    numreg = models.ForeignKey(Regions, to_field='numreg', on_delete=models.CASCADE)
+
+
 class Communes(models.Model):
     libcom = models.CharField(max_length=50, null=False)
     information_access = models.IntegerField(null=False)
     numeric_access = models.IntegerField(null=False)
     numeric_competence = models.IntegerField(null=False)
     administrative_competence = models.IntegerField(null=False)
-
-class Departements(models.Model):
-    libdep = models.CharField(max_length=50, null=False)
-    libcom = models.ForeignKey(Communes, on_delete=models.CASCADE)
-
-
-class Regions(models.Model):
-    libreg = models.CharField(max_length=50, null=False)
-    libdep = models.ForeignKey(Departements, on_delete=models.CASCADE)
+    numdep = models.ForeignKey(Departements, to_field='numdep', on_delete=models.CASCADE)

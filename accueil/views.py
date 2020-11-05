743e049750ebdd7e8999536eb233ca3cfa0ba3c4
Sylvain <boussignac.s@gmail.com>
2020-11-05 13:36:12 +0100

    moteur de recherche

--- accueil/views.py
+++ accueil/views.py
@@ -1,30 +1,47 @@
 from django.shortcuts import render
 from .forms import SearchForm
-from .models import Communes, Departements
+from .models import Communes, Departements, Regions
+
+
 # Create your views here.
 
 
 def index(request):
     form = SearchForm
+    regions = Regions.objects.all()
     context = {
-        'form': form
+        'form': form,
+        'regions': regions
     }
-    if request.method == 'POST':
-        form = SearchForm(request.POST, request.FILES)
 
+    if request.method == 'POST':
+        libreg = request.POST.get('regions')
+        libdep = request.POST.get('departements')
+        libcom = request.POST.get('communes')
+        form = SearchForm(request.POST, request.FILES)
+        results = None
         if form.is_valid():
             postalcode = form.cleaned_data['postalCode']
-            # region = form.cleaned_data['region']
-            # departement = form.cleaned_data['departement']
-            # commune = form.cleaned_data['commune']
 
-            results = None
+            print(postalcode)
 
             if postalcode != "":
-                results = Communes.objects.filter(code_postal=postalcode)
+                communes = Communes.objects.filter(code_postal=postalcode)
+                context['communes'] = communes
 
-         #  if departement != "":
-             #import re
-            # results = Communes.objects.filter(re.search(r'^Departements.numdep[0-9]{3}', Communes.code_postal))
+            if libreg != "":
+                region = Regions.objects.get(libreg=libreg)
+                departements = Departements.objects.filter(numreg_id=region.numreg)
+                context['departements'] = departements
+
+            if libdep != "":
+                departement = Departements.objects.get(libdep=libdep)
+                communes = Communes.objects.filter(numdep=departement.numdep)
+                context['communes'] = communes
+
+            if libcom != "":
+                results = Communes.objects.filter(libcom=libcom)
+                context['resultats'] = results
+
 
     return render(request, 'accueil/index.html', context)

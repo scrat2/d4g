c6743ade7e311a38d02d9300a0742e30afb8d6e8
Sylvain <boussignac.s@gmail.com>
2020-11-05 19:29:49 +0100

    maj pour prod

--- illectronisme/settings.py
+++ illectronisme/settings.py
@@ -128,6 +128,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    "/home/debian/projetd4g/illectronisme/accueil/static",
+]
+
 if os.environ.get('ENV') == 'PRODUCTION':
 
     # Static files settings
@@ -136,9 +140,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
     # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
-    )
+
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

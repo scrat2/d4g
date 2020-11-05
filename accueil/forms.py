7899210d64228fbbad2358909caaf515c8bc4779
Sylvain <boussignac.s@gmail.com>
2020-11-05 06:25:09 +0100

    recherche postalcode

---
+++ accueil/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+
+
+class SearchForm(forms.Form):
+
+    postalCode = forms.CharField(
+        label='PostalCode',
+        widget=forms.TextInput(attrs={'placeholder': 'Code postal'}),
+        max_length=5,
+        required=False
+    )
+
+    region = forms.CharField(
+        label='region',
+        widget=forms.TextInput(attrs={'placeholder': 'region'}),
+        max_length=50,
+        required=False
+    )
+
+    departement = forms.CharField(
+        label='departement',
+        widget=forms.TextInput(attrs={'placeholder': 'departement'}),
+        max_length=50,
+        required=False
+    )
+
+    commune = forms.CharField(
+        label='commune',
+        widget=forms.TextInput(attrs={'placeholder': 'commune'}),
+        max_length=50,
+        required=False
+    )
--- accueil/views.py
+++ accueil/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from .forms import SearchForm
+from .models import Communes
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'accueil/index.html')
+    form = SearchForm
+    context = {
+        'form': form
+    }
+    if request.method == 'POST':
+        form = SearchForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            postalcode = form.cleaned_data['postalCode']
+            # region = form.cleaned_data['region']
+            # departement = form.cleaned_data['departement']
+            # commune = form.cleaned_data['commune']
+
+            results = None
+
+            if postalcode != "":
+                results = Communes.objects.filter(code_postal=postalcode)
+
+    return render(request, 'accueil/index.html', context)
